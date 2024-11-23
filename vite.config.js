cfe633b0d28cd8a7975a248048a6a44571b80140
Daniil Zaburunov <zaburunov.2000@mail.ru>
2024-11-23 21:35:08 +0400

    add deploy.sh

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist",
     commonjsOptions: { transformMixedEsModules: true }, // Change
   },
-  base: "/my-pet-proj/",
+  base: "/my-pet-proj",
 });
