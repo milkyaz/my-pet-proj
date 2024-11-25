dbbf3673801a23d7bdf7fc961d5c9a9783c27079
Daniil Zaburunov <zaburunov.2000@mail.ru>
2024-11-25 22:38:00 +0400

    fix

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,6 +1,5 @@
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
